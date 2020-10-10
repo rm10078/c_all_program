0eef26e9be4494dd24fc6edfb185745b663ed1e8
rajib <rm10078@gmail.com>
2020-10-10 20:03:45 +0530

    change 21 and 22 files

--- 21_c_array.c
+++ 21_c_array.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+//------------------one dimensional array--------------------------
 int main() {
-int a[]={10,11,12,19,17,15};
+int a[]={10,11,12,19,17,15}; // a is a array variable
 for(int i=0;i<6;i++){
 printf("\na[%d] value is %d",i,a[i]);
 }
